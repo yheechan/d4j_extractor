b12eaeaa6256336109c97d837ae207bf7ce899d4
Yang Heechan <heechy06@naver.com>
2025-08-10 03:15:37 -0700

    Implemented tcs saving for finer grained mutation testing

--- lib/saver_engine.py
+++ lib/saver_engine.py
@@ -35,41 +35,47 @@ class SaverEngine:
         self.SUBJECTINFO_DIR = f"{self.RESULT_DIR}/subjectInfo"
 
     def run(self):
-        # self.save_fault()
+        self.save_fault()
         self.save_line_info()
-        # self.save_tc_info()
+        self.save_tc_info()
         # self.save_mutation_info()
         # self.zip_out_dir()
     
     def save_fault(self):
         values = [self.PID, self.BID, self.EL]
-        self.DB.insert(
-            "d4j_fault_info",
-            "project, bug_id, experiment_label",
-            values
-        )
+        self.fault_idx = 999
+        # self.DB.insert(
+        #     "d4j_fault_info",
+        #     "project, bug_id, experiment_label",
+        #     values
+        # )
 
-        self.fault_idx = self.DB.read(
-            "d4j_fault_info",
-            columns="fault_idx",
-            conditions={
-                "project": self.PID,
-                "bug_id": self.BID,
-                "experiment_label": self.EL
-            }
-        )
+        # self.fault_idx = self.DB.read(
+        #     "d4j_fault_info",
+        #     columns="fault_idx",
+        #     conditions={
+        #         "project": self.PID,
+        #         "bug_id": self.BID,
+        #         "experiment_label": self.EL
+        #     }
+        # )
 
-        if not self.fault_idx:
-            LOGGER.error(f"Failed to retrieve fault index for subject {self.PID}, bug ID {self.BID}, experiment label {self.EL}.")
-            return
-        self.fault_idx = self.fault_idx[0][0]
-        LOGGER.info(f"Fault information saved for subject {self.PID}, bug ID {self.BID}, experiment label {self.EL}.")
-    
+        # if not self.fault_idx:
+        #     LOGGER.error(f"Failed to retrieve fault index for subject {self.PID}, bug ID {self.BID}, experiment label {self.EL}.")
+        #     return
+        # self.fault_idx = self.fault_idx[0][0]
+        LOGGER.info(f"Fault information saved for subject {self.PID}, bug ID {self.BID}, experiment label {self.EL} with fault index {self.fault_idx}.")
+
     def save_line_info(self):
         unique_line_idx = -1
-        line_data = {}
         self.lineInfo2lineIdx = {}
+        self.class2lineInfo = {}
+
         for class_report in os.listdir(self.PERFILEREPORT_DIR):
+            target_class = class_report.split("-")[0]
+            if target_class not in self.class2lineInfo:
+                self.class2lineInfo[target_class] = {}
+
             line_info_csv = os.path.join(self.PERFILEREPORT_DIR, class_report, "line_info.csv")
             with open(line_info_csv, 'r') as csvfile:
                 reader = csv.reader(csvfile)
@@ -84,17 +90,27 @@ class SaverEngine:
                     method_name = line_info.split("#")[1].split(":")[0]
                     line_num = line_info.split("#")[1].split(":")[1]
 
-                    # save line_data for unique lines
-                    if code_filename not in line_data:
-                        line_data[code_filename] = {}
-                    if class_name not in line_data[code_filename]:
-                        line_data[code_filename][class_name] = {}
-                    if method_name not in line_data[code_filename][class_name]:
-                        line_data[code_filename][class_name][method_name] = []
-                    if line_num not in line_data[code_filename][class_name][method_name]:
-                        line_data[code_filename][class_name][method_name].append(line_num)
-                        unique_line_idx += 1
+                    self.class2lineInfo[target_class][line_info] = {
+                        "line_idx": line_id,
+                        "file": code_filename,
+                        "class": class_name,
+                        "method": method_name,
+                        "line_num": line_num
+                    }
 
+                    # save project_line_data for unique lines for DB
+                    if line_info not in self.lineInfo2lineIdx:
+                        unique_line_idx += 1
+                        # Store the line information to LINEINFO2LINEIDX
+                        self.lineInfo2lineIdx[line_info] = {
+                            "line_idx": unique_line_idx,
+                            "file": code_filename,
+                            "class": class_name,
+                            "method": method_name,
+                            "line_num": line_num
+                        }
+
+                        # Save the line information to DB
                         values = [
                             unique_line_idx, code_filename, class_name, method_name, line_num
                         ]
@@ -104,31 +120,30 @@ class SaverEngine:
                         #     values
                         # )
 
-                        self.lineInfo2lineIdx[line_info] = {
-                            "line_idx": unique_line_idx,
-                            "file": code_filename,
-                            "class": class_name,
-                            "method": method_name,
-                            "line_num": line_num
-                        }
     
         LOGGER.info(f"Save {unique_line_idx+1} lines for subject {self.PID}, bug ID {self.BID}, experiment label {self.EL}.")
 
     def save_tc_info(self):
-        BASELINE_TEST_RESULTS_DIR = f"{self.OUT_DIR}/baselineTestResults"
-        if not os.path.exists(BASELINE_TEST_RESULTS_DIR):
-            LOGGER.warning(f"Baseline test results directory {BASELINE_TEST_RESULTS_DIR} does not exist.")
-            return
+        unique_tc_idx = -1
+        self.project_tcs_data = {}
+        self.class2tcsInfo = {}
+        failing_tcs_count = 0
+        passing_tcs_count = 0
 
-        # Save test case information to the database
-        for result_file in os.listdir(BASELINE_TEST_RESULTS_DIR):
-            if result_file.endswith(".json"):
-                # e.g., result_file = "0_test_results.json"
-                file_idx = result_file.split("_")[0]
-                result_path = os.path.join(BASELINE_TEST_RESULTS_DIR, result_file)
+        for class_report in os.listdir(self.PERFILEREPORT_DIR):
+            target_class = class_report.split("-")[0]
+            if target_class not in self.class2tcsInfo:
+                self.class2tcsInfo[target_class] = {}
+
+            baseline_test_results_dir = os.path.join(self.PERFILEREPORT_DIR, class_report, "baselineTestResults")
+            for result_file in os.listdir(baseline_test_results_dir):
+                if not result_file.endswith(".json"):
+                    continue
+                result_path = os.path.join(baseline_test_results_dir, result_file)
                 with open(result_path, 'r') as f:
                     tc_data = json.load(f)
 
+                    # Get the test case information
                     tc_idx = tc_data["test_info"]["test_id"]
                     test_name = tc_data["test_info"]["test_name"]
                     result = 1 if tc_data["test_info"]["result"] == "FAIL" else 0
@@ -141,20 +156,61 @@ class SaverEngine:
                     exception_msg = tc_data["exception"]["message"]
                     stacktrace = tc_data["exception"]["stack_trace"]
 
-                    values = [
-                        self.fault_idx, tc_idx, test_name, result,
-                        execution_time_ms, bit_sequence_length,
-                        line_coverage_bit_sequence, exception_type,
-                        exception_msg, stacktrace
-                    ]
-                    self.DB.insert(
-                        "d4j_tc_info",
-                        "fault_idx, tc_idx, test_name, result, execution_time_ms, bit_sequence_length, line_coverage_bit_sequence, exception_type, exception_msg, stacktrace",
-                        values
-                    )
+                    # Store the test case information in class2tcsInfo
+                    if test_name not in self.class2tcsInfo[target_class]:
+                        self.class2tcsInfo[target_class][test_name] = {
+                            "tc_idx": tc_idx,
+                            "result": result,
+                            "execution_time_ms": execution_time_ms,
+                            "bit_sequence_length": bit_sequence_length,
+                            "line_coverage_bit_sequence": line_coverage_bit_sequence,
+                            "exception_type": exception_type,
+                            "exception_msg": exception_msg,
+                            "stacktrace": stacktrace
+                        }
+                    
+                    # Save the unique test case data
+                    if test_name not in self.project_tcs_data:
+                        unique_tc_idx += 1
+                        self.project_tcs_data[test_name] = {
+                            "tc_idx": unique_tc_idx,
+                            "result": result,
+                            "execution_time_ms": execution_time_ms,
+                            "bit_sequence_length": bit_sequence_length,
+                            "line_coverage_bit_sequence": line_coverage_bit_sequence,
+                            "exception_type": exception_type,
+                            "exception_msg": exception_msg,
+                            "stacktrace": stacktrace
+                        }
+                        values = [
+                            self.fault_idx, unique_tc_idx, test_name, result,
+                            execution_time_ms, bit_sequence_length,
+                            line_coverage_bit_sequence, exception_type,
+                            exception_msg, stacktrace
+                        ]
+                        # self.DB.insert(
+                        #     "d4j_tc_info",
+                        #     "fault_idx, tc_idx, test_name, result, execution_time_ms, bit_sequence_length, line_coverage_bit_sequence, exception_type, exception_msg, stacktrace",
+                        #     values
+                        # )
+                        if result == 1:
+                            failing_tcs_count += 1
+                        else:
+                            passing_tcs_count += 1
+                    elif test_name in self.project_tcs_data and execution_time_ms > self.project_tcs_data[test_name]["execution_time_ms"]:
+                        # Update the existing test case data with the new one if it has a longer execution time
+                        self.project_tcs_data[test_name]["execution_time_ms"] = execution_time_ms
+                        # self.DB.update(
+                        #     "d4j_tc_info",
+                        #     set_values={"execution_time_ms": execution_time_ms},
+                        #     conditions={
+                        #         "fault_idx": self.fault_idx,
+                        #         "tc_idx": self.project_tcs_data[test_name]["tc_idx"]
+                        #     }
+                        # )
 
-        LOGGER.info(f"Test case information saved for subject {self.PID}, bug ID {self.BID}, experiment label {self.EL}.")
-    
+        LOGGER.info(f"Save {unique_tc_idx+1} tcs information for subject {self.PID}, bug ID {self.BID}, experiment label {self.EL}.")
+        LOGGER.info(f"Total failing tcs: {failing_tcs_count}, passing tcs: {passing_tcs_count} for subject {self.PID}, bug ID {self.BID}, experiment label {self.EL}.")
 
     def save_mutation_info(self):
         full_MUTATION_MATRIX_CSV = f"{self.OUT_DIR}/full_mutation_matrix.csv"
